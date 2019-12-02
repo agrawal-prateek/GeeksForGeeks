d9a7c36edb69f17312a6e596e958378b6fe13fec
Prateek Agrawal <prateekagrawal89760@gmail.com>
2019-12-03 01:37:22 +0530

    Time: O(k + k log k + (n - k) log k), space: O(k)

--- cpp/Kth Largest Element.cpp
+++ cpp/Kth Largest Element.cpp
@@ -1,3 +1,31 @@
+/*
+Given an array of N positive integers, print k largest elements from the array.  The output elements should be printed in decreasing order.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The first line of each test case is N and k, N is the size of array and K is the largest elements to be returned. The second line of each test case contains N input C[i].
+
+Output:
+Print the k largest element in descending order.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 100
+K ≤ N
+1 ≤ C[i] ≤ 1000
+
+Example:
+Input:
+2
+5 2
+12 5 787 1 23
+7 3
+1 23 12 9 30 2 50
+
+Output:
+787 23
+50 30 23
+*/
+
 #include<iostream>
 #include<vector>
 #include<limits>
