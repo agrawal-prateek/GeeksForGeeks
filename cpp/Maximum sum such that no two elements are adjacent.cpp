1d99635b6f21d13c4dc7a0b4bb20f45c006390c2
Prateek Agrawal <prateekagrawal89760@gmail.com>
2019-12-03 21:33:48 +0530

    Time: O(N), Space: O(1)

--- cpp/Maximum sum such that no two elements are adjacent.cpp
+++ cpp/Maximum sum such that no two elements are adjacent.cpp
@@ -1,3 +1,34 @@
+/**
+Given an array A of positive integers. Find the maximum sum of a subsequence
+such that no two numbers in the sequence should be adjacent in the array.
+
+Input:
+The first line of input contains an integer T denoting the number of test 
+cases. The first line of each test case is N, size of array. The second line of 
+each test case contains N elements.
+
+Output:
+Print the maximum sum of a subsequence.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 106
+1 ≤ Ci ≤ 107
+
+Example:
+Input:
+2
+6
+5 5 10 100 10 5
+4
+3 2 7 10
+
+Output:
+110
+13
+*/
+
+
 #include<iostream>
 using namespace std;
 
